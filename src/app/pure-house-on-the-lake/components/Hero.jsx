aac2c17e20ab947495ebd260ca72b473a5c93c47
Youssef97dev <youssefmoussafi97@gmail.com>
2025-10-08 19:37:19 +0100

    delete logo lake

--- src/app/pure-house-on-the-lake/components/Hero.jsx
+++ src/app/pure-house-on-the-lake/components/Hero.jsx
@@ -93,18 +93,6 @@ const Hero = () => {
       {/* Overlay */}
       <div className="absolute inset-0 filter sepia contrast-125"></div>
 
-      <div className="absolute top-36 w-full h-full flex justify-center items-center z-50">
-        <Link href="/pure-house-on-the-lake">
-          <Image
-            src={"/lake/lake-logo-white-v2.png"}
-            width={300}
-            height={300}
-            alt="Logo pure house on the lake"
-            className={`mt-2 w-32 lg:w-60`}
-          />
-        </Link>
-      </div>
-
       <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 text-white z-50">
         <a href="#about">
           <svg
