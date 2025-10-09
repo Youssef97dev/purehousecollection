e7fc11f4e4ac904761e76e202231e84bce203447
Youssef97dev <youssefmoussafi97@gmail.com>
2025-10-09 15:44:40 +0100

    change pics 3

--- src/app/pure-house-on-the-lake/components/Hero.jsx
+++ src/app/pure-house-on-the-lake/components/Hero.jsx
@@ -27,12 +27,13 @@ const imagesSmall = [
     src: "https://res.cloudinary.com/dz7wroord/image/upload/v1760018827/PureHouseOnTheLake/IMG_9215_rx4fop.jpg",
     class: "object-center",
   },
+
   {
-    src: "/lake/on-the-lake-small-1.jpg",
+    src: "https://res.cloudinary.com/dz7wroord/image/upload/v1760018827/PureHouseOnTheLake/IMG_9213_tpkt83.jpg",
     class: "object-center",
   },
   {
-    src: "https://res.cloudinary.com/dz7wroord/image/upload/v1760018827/PureHouseOnTheLake/IMG_9213_tpkt83.jpg",
+    src: "/lake/on-the-lake-small-1.jpg",
     class: "object-center",
   },
   {
