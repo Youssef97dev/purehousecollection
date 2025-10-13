b20f93ca6c308bc271aebee2429969b57226b614
Youssef97dev <youssefmoussafi97@gmail.com>
2025-10-13 02:45:23 +0100

    change gallery on the lake

--- src/app/pure-house-on-the-lake/gallery/components/Gallery.jsx
+++ src/app/pure-house-on-the-lake/gallery/components/Gallery.jsx
@@ -4,113 +4,71 @@ import Link from "next/link";
 
 const GalleryImage = [
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1760018827/PureHouseOnTheLake/IMG_9215_rx4fop.jpg",
+    image: "/lake/IMG_9215.jpeg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1760018827/PureHouseOnTheLake/IMG_9216_ofrzle.jpg",
+    image: "/lake/gallery/lake-41.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1760018827/PureHouseOnTheLake/IMG_9213_tpkt83.jpg",
+    image: "/lake/IMG_9213.jpeg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1760018828/PureHouseOnTheLake/IMG_9217_lpyvj5.jpg",
+    image: "/lake/IMG_9217.jpeg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1760018829/PureHouseOnTheLake/IMG_9358_sgssol.jpg",
+    image: "/lake/IMG_9358.jpeg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1760018830/PureHouseOnTheLake/IMG_9407_dcvqqk.jpg",
+    image: "/lake/IMG_9407.jpeg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1760018830/PureHouseOnTheLake/IMG_9362_vckvjw.jpg",
+    image: "/lake/gallery/lake-42.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765784/PureHouseOnTheLake/lake-23_pkd4tn.jpg",
+    image: "/lake/on-the-lake-small-1.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765784/PureHouseOnTheLake/lake-25_tevypz.jpg",
+    image: "/lake/gallery/lake-25.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765792/PureHouseOnTheLake/lake-37_npii9v.jpg",
+    image: "/lake/lake-37.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765800/PureHouseOnTheLake/lake-40_oxtnwg.jpg",
+    image: "/lake/gallery/lake-40.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765798/PureHouseOnTheLake/lake-39_kcbn1w.jpg",
+    image: "/lake/lake-33.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765791/PureHouseOnTheLake/lake-33_uz9gb3.jpg",
+    image: "/lake/gallery/lake-31.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765791/PureHouseOnTheLake/lake-31_gvqtcz.jpg",
+    image: "/lake/gallery/lake-26.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765784/PureHouseOnTheLake/lake-26_b191yh.jpg",
+    image: "/lake/gallery/lake-15.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765777/PureHouseOnTheLake/lake-15_lyd03s.jpg",
+    image: "/lake/gallery/lake-5.jpg",
     class: "object-center",
   },
   {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765776/PureHouseOnTheLake/lake-5_e9lluo.jpg",
-    class: "object-center",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765776/PureHouseOnTheLake/lake-6_kfn1v1.jpg",
-    class: "object-center",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765776/PureHouseOnTheLake/lake-11_siqhxl.jpg",
-    class: "object-center",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765776/PureHouseOnTheLake/lake-1_elty8o.jpg",
-    class: "object-center",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765776/PureHouseOnTheLake/lake-8_fadumq.jpg",
-    class: "object-center",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dz7wroord/image/upload/v1759765776/PureHouseOnTheLake/lake-4_heejht.jpg",
+    image: "/lake/gallery/lake-6.jpg",
     class: "object-center",
   },
 ];
